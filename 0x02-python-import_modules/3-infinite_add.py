fa16780aef341af77fa385e066e3b9c3033b6a88
Aximoo <tochukwuodoh03@gmail.com>
2023-06-08 04:36:09 -0700

    add.py3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """addition"""
-from sys import argv
-add = 0
-for s in argv[1:]:
-    add += int(s)
-print("{}".format(add))
+if __name__ == "__main__":
+    from sys import argv
+    add = 0
+    for s in argv[1:]:
+        add += int(s)
+        print("{}".format(add))
